1ab972db10cfc278e2688706626517230a33afd9
Claudia686 <claudia_mihaela@rocketmail.com>
2025-04-27 09:40:54 +0100

    install forge-std

--- script/Crowdfunding.s.sol
+++ script/Crowdfunding.s.sol
@@ -1,1 +1,18 @@
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
+
+// import {Script} from "forge-std/Script.sol";
+// import {Crowdfunding} from "../src/Crowdfunding.sol";
+
+// contract DeplotCrowdfunding is Script {
+//     function run() external {
+//         address public USER = makeAddr("user");
+//         address public OWNER = makeAddr("owner");
+
+//         vm.startBroadcast();
+//         crowdfunding = new Crowdfunding(10e18, 10 days);
+//         vm.stopBroadcast();
+
+//     }
+// }
 
